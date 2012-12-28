117cd7a3317d9c38c5b71ef4a91efda4fb68bd31
Noam Camus <noamc@ezchip.com>
2012-12-28 11:24:53 +0200

    ARC: [plat-eznps] Add device for Data Plane Linux
    Add new device /dev/dpl.
    This device enable access to cmem in other cores.
    
    It is usefull for debugging when gdbserver is attached to process
    which runs in a different CPU than gdbserver.
    
    Signed-off-by: Noam Camus <noamc@ezchip.com>
    Signed-off-by: Tal Zilcer <talz@ezchip.com>

---
+++ arch/arc/plat-eznps/include/plat/ctop.h
@@ -0,0 +1,27 @@
+/*******************************************************************************
+
+  Copyright(c) 2012 EZchip Technologies.
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms and conditions of the GNU General Public License,
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  The full GNU General Public License is included in this distribution in
+  the file called "COPYING".
+
+*******************************************************************************/
+
+#ifndef _PLAT_EZNPS_CTOP_H
+#define _PLAT_EZNPS_CTOP_H
+
+
+/* core auxiliary registers */
+#define CTOP_AUX_BASE		0xfffff800
+
+
+#endif /* _PLAT_EZNPS_CTOP_H */
