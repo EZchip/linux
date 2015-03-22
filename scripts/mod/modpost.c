9cc8ca2ab4149eacd3836e8aafe7426fb5aee54d
Noam Camus <noamc@ezchip.com>
2015-03-22 23:06:34 +0200

    scripts: [modpost] add new sections to white list
    Adding cmem, cmem_shared, and fmt_slot* to list
    
    Signed-off-by: Noam Camus <noamc@ezchip.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -833,6 +833,8 @@ static const char *const section_white_list[] =
 	".xt.lit",         /* xtensa */
 	".arcextmap*",			/* arc */
 	".gnu.linkonce.arcext*",	/* arc : modules */
+	".cmem*",			/* EZchip */
+	".fmt_slot*",			/* EZchip */
 	".gnu.lto*",
 	NULL
 };
